fa60d15d9a30c03f7363b3aba4b6cb22c25d471a
Jhon Gomez <jhonjairogomez92@gmail.com>
2022-11-17 19:44:01 -0500

    Add searcher

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import React from "react";
+import { Container } from '@mui/material';
+import Searcher from "./components/Searcher";
 
 const App = () => {
   return(
-    <div>Soy un platzinauta</div>
+    <Container sx={{
+      background: 'whiteSmoke',
+      width: '80vw',
+      height: '500px',
+      borderRadius: '16px',
+      marginTop: '40px',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+
+    }}>
+      <Searcher></Searcher>
+    </Container>
   )
 };
 
---
+++ src/components/Searcher/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { IconButton, Stack, TextField } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+
+
+const Searcher = () => {
+  return(
+    <Stack
+      direction= 'row'
+      sx={{
+        marginTop: '30px',
+        width: '80%'
+      }}
+    >
+      <TextField
+        id="outlined-basic"
+        variant="outlined"
+        label="GitHub User"
+        placeholder="Buscar Usuario de GitHub"
+        size="small"
+        sx={{
+          width: '90%',
+          }}
+      />
+      <IconButton
+        size="small"
+        sx={{
+        left: '-45px'
+      }}
+      >
+        <SearchIcon />
+      </IconButton>
+    </Stack>
+  )
+}
+
+export default Searcher
