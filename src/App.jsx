233bb5ab743a119f0e35be593276c536bb194bd8
Jhon Gomez <jhonjairogomez92@gmail.com>
2022-11-18 10:34:55 -0500

    Add conection with GitHub API

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container } from '@mui/material';
 import Searcher from "./components/Searcher";
+import { getGitHubUser } from "./services/users";
 
 const App = () => {
   const [ inputUser, setInputUser ] = React.useState('octocat');
   // const [ userState, setUserState ] = useState('inputUser');
+
+  const gettingUser = async (user) => {
+    const userResponse = await getGitHubUser(user);
+    console.log(userResponse);
+  }
+
+  useEffect(() => {
+    gettingUser(inputUser)
+  },[])
   return(
     <Container sx={{
       background: 'whiteSmoke',
